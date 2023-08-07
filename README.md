b8cb550ae76c8f2230ad294e729ccb942f29eb9c
yoohunn <poirtmove@gmail.com>
2023-08-07 10:31:29 +0900

    ✨ Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+## 🥯 Bread Clock
---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import './globals.css';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: { default: '배고빵', template: '%s | 배고빵' },
+  description: '‘빵시계’는 갓구운 빵을 찾는 사람들을 위한 서비스입니다 ;)',
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang='en'>
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
